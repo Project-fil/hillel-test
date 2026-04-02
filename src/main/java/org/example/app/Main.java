385eafa33c3210e2f1262bcb095c29c64d1fb23f
andryfil <filya1234321@ukr.net>
2026-04-02 20:28:14 +0300

    first commit

---
+++ src/main/java/org/example/app/Main.java
@@ -0,0 +1,16 @@
+package org.example.app;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 7;
+
+        System.out.println(i + j);
+        System.out.println(i - j);
+        System.out.println(i * j);
+        System.out.println(i / j);
+
+    }
+
+}
